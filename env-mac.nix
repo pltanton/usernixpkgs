31d1ccaeafb2b7442f09cdac5a60cf0439b7285b
Anton Plotnikov <apl@dsx.uk>
2019-12-10 12:21:05 +0300

    Fix mac env

--- env-mac.nix
+++ env-mac.nix
@@ -4,6 +4,6 @@ pkgs:
   programs = import ./modules/programs/mac.nix pkgs;
   packages = import ./modules/packages/mac.nix pkgs;
   files = colors: {};
-  extraHome = {};
+  extraHome = oldAttrs: {};
   services = {};
 }
